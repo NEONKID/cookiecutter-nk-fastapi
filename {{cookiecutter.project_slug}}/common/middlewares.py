31fe3fb551ce312d1864e3df96e7195fba42d268
NEONKID <contact@neonkid.xyz>
2021-09-23 15:58:46 +0900

    Change package name..

--- {{cookiecutter.project_slug}}/common/middlewares.py
+++
@@ -1,6 +0,0 @@
-def add_middlewares(middlewares, app):
-    from loguru import logger
-
-    for middleware in middlewares:
-        app.add_middleware(middleware)
-        logger.debug("Add middleware: {}".format(middleware.__name__))
